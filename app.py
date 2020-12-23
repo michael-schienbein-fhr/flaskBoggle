9246004acd38a2bc0f6b7eeb934bcfed1afd221f
Michael Schienbein <moomba0@gmail.com>
2020-12-23 17:00:38 +0300

    completed project

---
+++ app.py
@@ -0,0 +1,53 @@
+from boggle import Boggle
+from flask import Flask, request, render_template, jsonify, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = 'key'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+boggle_game = Boggle()
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/game')
+def render_game():
+    board = boggle_game.make_board()
+    session['board'] = board
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+    return render_template("game.html", board=board,
+                           highscore=highscore,
+                           nplays=nplays)
+
+
+@app.route("/check-word")
+def check_word():
+    """Check if word is in dictionary."""
+
+    word = request.args["word"]
+    board = session["board"]
+    response = boggle_game.check_valid_word(board, word)
+
+    return jsonify({'result': response})
+
+
+@app.route("/post-score", methods=["POST"])
+def post_score():
+    """Receive score, update nplays, update high score if appropriate."""
+
+    score = request.json["score"]
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    session['nplays'] = nplays + 1
+    session['highscore'] = max(score, highscore)
+
+    return jsonify(brokeRecord=score > highscore)
---
+++ test_app.py
@@ -0,0 +1,52 @@
+from app import app
+from flask import session
+from unittest import TestCase
+
+
+class FlaskTests(TestCase):
+
+    def setUp(self):
+        """Stuff to do before every test."""
+
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+
+    def test_homepage(self):
+        """Make sure information is in the session and HTML is displayed"""
+
+        with self.client:
+            response = self.client.get('/game')
+            self.assertIn('board', session)
+            self.assertIsNone(session.get('highscore'))
+            self.assertIsNone(session.get('nplays'))
+            self.assertIn(b'High Score:', response.data)
+            self.assertIn(b'Score:', response.data)
+            self.assertIn(b'Seconds Left:', response.data)
+
+    def test_valid_word(self):
+        """Test if word is valid by modifying the board in the session"""
+
+        with self.client as client:
+            with client.session_transaction() as sess:
+                sess['board'] = [["C", "A", "T", "T", "T"],
+                                 ["C", "A", "T", "T", "T"],
+                                 ["C", "A", "T", "T", "T"],
+                                 ["C", "A", "T", "T", "T"],
+                                 ["C", "A", "T", "T", "T"]]
+        response = self.client.get('/check-word?word=cat')
+        self.assertEqual(response.json['result'], 'ok')
+
+    def test_invalid_word(self):
+        """Test if word is in the dictionary"""
+
+        self.client.get('/game')
+        response = self.client.get('/check-word?word=impossible')
+        self.assertEqual(response.json['result'], 'not-on-board')
+
+    def non_english_word(self):
+        """Test if word is on the board"""
+
+        self.client.get('/game')
+        response = self.client.get(
+            '/check-word?word=aoeuhtsaoeuhtsaoeuhs')
+        self.assertEqual(response.json['result'], 'not-word')
